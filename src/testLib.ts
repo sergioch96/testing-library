03f828f6a781e7c4e701cd38fa212eb7c19dd330
Sergio Chamorro <sergiochamo96@gmail.com>
2025-10-27 20:22:52 -0300

    add basic statistics functions and testing framework

---
+++ src/testLib.ts
@@ -0,0 +1,45 @@
+export function expect<T>(expected: T) {
+  return {
+    toBe(result: T) {
+      if (result !== expected) {
+        throw new Error(`❌: ${result} is not equal to ${expected}`);
+      }
+    },
+  };
+}
+
+export function describe(description: string, callback: () => void) {
+  console.log(description);
+  callback();
+}
+
+export function test(
+  description: string,
+  callback: () => void | Promise<void>
+) {
+  try {
+    isPromise(callback())
+      ? evaluatePromise(description, callback as () => Promise<void>)
+      : evaluate(description, callback);
+  } catch (error) {
+    console.log(`❌ ${description}`);
+    console.log(error);
+  }
+}
+
+function evaluate(description: string, callback: () => void) {
+  callback();
+  console.log(`✅ ${description}`);
+}
+
+function evaluatePromise(description: string, callback: () => Promise<void>) {
+  callback()
+    .then(() => console.log(`✅ ${description}`))
+    .catch((error) => console.log(`❌ ${description} ${error}`));
+}
+
+function isPromise(promise) {
+  return !!promise && typeof promise.then === "function";
+}
+
+export const it = test;
