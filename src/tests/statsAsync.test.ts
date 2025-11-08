e3448b1c9a63f2e1373ae05881a128b8a9df8705
Sergio Chamorro <sergiochamo96@gmail.com>
2025-11-08 09:23:39 -0300

    remove skip

--- src/tests/statsAsync.test.ts
+++ src/tests/statsAsync.test.ts
@@ -1,7 +1,7 @@
 import * as statsAsync from "../statsAsync";
 
 describe("Async Stats should", () => {
-  it.only("calculates the sum of all elements of the array (async)", async () => {
+  it("calculates the sum of all elements of the array (async)", async () => {
     const result = await statsAsync.sum([1, 2, 3]);
     const expected = 6;
     expect(expected).toBe(result);
