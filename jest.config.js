23c8f3ea313ca6a77a6580ad75ca2e29a9c84378
Sergio Chamorro <sergiochamo96@gmail.com>
2025-11-08 15:07:51 -0300

    Add code coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,21 @@ module.exports = {
   transform: {
     ...tsJestTransformCfg,
   },
+  collectCoverage: true,
+  coverageDirectory: "./coverage",
+  coverageReporters: ["json", "text", "lcov", "clover"],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+    "./src/stats.ts": {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
